01b5491d8782fd807227afb24f2358c2974acad6
Mohammad Talha Bin Haneef <talhahaneef560@gmail.com>
2022-03-05 00:31:01 +0500

    added

--- LodhranUniversity/Controllers/CoursesController.cs
+++ LodhranUniversity/Controllers/CoursesController.cs
@@ -7,7 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using LodhranUniversity.Data;
-
+using System.Data.SqlClient;
 namespace LodhranUniversity.Controllers
 {
     public class CoursesController : Controller
